4f91c30c47137eb345db5439598eef30e249a2ac
karthikmohan14 <karthikmkochi@gmail.com>
2018-07-26 12:43:59 +0530

    nested menu

--- class13Menu/app/src/main/java/com/example/karthik/class13menu/MainActivity.java
+++ class13Menu/app/src/main/java/com/example/karthik/class13menu/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        
+        switch(item.getItemId()){
+            case R.id.help:
+                Toast.makeText(this, "i wont give up", Toast.LENGTH_SHORT).show();
+            case R.id.help2:
+                Toast.makeText(this, "i wont give up again", Toast.LENGTH_SHORT).show();
+            case R.id.help3:
+                Toast.makeText(this, "let me see", Toast.LENGTH_SHORT).show();
+        }return true;
     }
 }
