191902d7b94e015ed774a6201d679a862a0e6dfa
karthikmohan14 <karthikmkochi@gmail.com>
2018-07-26 12:39:28 +0530

    menu xml

---
+++ class13Menu/app/src/main/java/com/example/karthik/class13menu/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.karthik.class13menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        
+    }
+}
